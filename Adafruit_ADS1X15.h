244344202170814c6c69c03f27233657673f621e
Tobias Bildner <tobiasbildner@gmail.com>
2023-06-29 19:24:55 +0200

    .

--- Adafruit_ADS1X15.h
+++ Adafruit_ADS1X15.h
@@ -25,21 +25,21 @@
     I2C ADDRESS/BITS
     -----------------------------------------------------------------------*/
 #define ADS1X15_ADDRESS (0x48) ///< 1001 000 (ADDR = GND)
-    /*=========================================================================*/
+/*=========================================================================*/
 
-    /*=========================================================================
-        POINTER REGISTER
-        -----------------------------------------------------------------------*/
+/*=========================================================================
+    POINTER REGISTER
+    -----------------------------------------------------------------------*/
 #define ADS1X15_REG_POINTER_MASK (0x03)      ///< Point mask
 #define ADS1X15_REG_POINTER_CONVERT (0x00)   ///< Conversion
 #define ADS1X15_REG_POINTER_CONFIG (0x01)    ///< Configuration
 #define ADS1X15_REG_POINTER_LOWTHRESH (0x02) ///< Low threshold
 #define ADS1X15_REG_POINTER_HITHRESH (0x03)  ///< High threshold
-        /*=========================================================================*/
+/*=========================================================================*/
 
-        /*=========================================================================
-            CONFIG REGISTER
-            -----------------------------------------------------------------------*/
+/*=========================================================================
+    CONFIG REGISTER
+    -----------------------------------------------------------------------*/
 #define ADS1X15_REG_CONFIG_OS_MASK (0x8000) ///< OS Mask
 #define ADS1X15_REG_CONFIG_OS_SINGLE                                           \
   (0x8000) ///< Write: Set to start a single-conversion
@@ -115,12 +115,12 @@ constexpr uint16_t MUX_BY_CHANNEL[] = {
 
 /** Gain settings */
 typedef enum {
-    GAIN_TWOTHIRDS = ADS1X15_REG_CONFIG_PGA_6_144V,
-    GAIN_ONE = ADS1X15_REG_CONFIG_PGA_4_096V,
-    GAIN_TWO = ADS1X15_REG_CONFIG_PGA_2_048V,
-    GAIN_FOUR = ADS1X15_REG_CONFIG_PGA_1_024V,
-    GAIN_EIGHT = ADS1X15_REG_CONFIG_PGA_0_512V,
-    GAIN_SIXTEEN = ADS1X15_REG_CONFIG_PGA_0_256V
+  GAIN_TWOTHIRDS = ADS1X15_REG_CONFIG_PGA_6_144V,
+  GAIN_ONE = ADS1X15_REG_CONFIG_PGA_4_096V,
+  GAIN_TWO = ADS1X15_REG_CONFIG_PGA_2_048V,
+  GAIN_FOUR = ADS1X15_REG_CONFIG_PGA_1_024V,
+  GAIN_EIGHT = ADS1X15_REG_CONFIG_PGA_0_512V,
+  GAIN_SIXTEEN = ADS1X15_REG_CONFIG_PGA_0_256V
 } adsGain_t;
 
 /** Data rates */
@@ -148,41 +148,39 @@ typedef enum {
 /**************************************************************************/
 class Adafruit_ADS1X15 {
 protected:
-    // Instance-specific properties
-    Adafruit_I2CDevice* m_i2c_dev; ///< I2C bus device
-    uint8_t m_bitShift;            ///< bit shift amount
-    adsGain_t m_gain;              ///< ADC gain
-    uint16_t m_dataRate;           ///< Data rate
+  // Instance-specific properties
+  Adafruit_I2CDevice *m_i2c_dev; ///< I2C bus device
+  uint8_t m_bitShift;            ///< bit shift amount
+  adsGain_t m_gain;              ///< ADC gain
+  uint16_t m_dataRate;           ///< Data rate
 
 public:
-    bool begin(uint8_t i2c_addr = ADS1X15_ADDRESS, TwoWire* wire = &Wire);
+  bool begin(uint8_t i2c_addr = ADS1X15_ADDRESS, TwoWire *wire = &Wire);
+  virtual bool begin(SoftI2C* wire, uint8_t i2c_addr = ADS1X15_ADDRESS) {
+    this->m_i2c_dev = new Adafruit_I2CDeviceSoft(i2c_addr, wire);
+    return (this->m_i2c_dev->begin());
+  };
+  int16_t readADC_SingleEnded(uint8_t channel);
+  int16_t readADC_Differential_0_1();
+  int16_t readADC_Differential_0_3();
+  int16_t readADC_Differential_1_3();
+  int16_t readADC_Differential_2_3();
+  void startComparator_SingleEnded(uint8_t channel, int16_t threshold);
+  int16_t getLastConversionResults();
+  float computeVolts(int16_t counts);
+  void setGain(adsGain_t gain);
+  adsGain_t getGain();
+  void setDataRate(uint16_t rate);
+  uint16_t getDataRate();
 
-    virtual bool begin(SoftI2C* wire, uint8_t i2c_addr = ADS1X15_ADDRESS) {
-        this->m_i2c_dev = new Adafruit_I2CDeviceSoft(i2c_addr, wire);
-        return (this->m_i2c_dev->begin());
-    };
+  void startADCReading(uint16_t mux, bool continuous);
 
-    int16_t readADC_SingleEnded(uint8_t channel);
-    int16_t readADC_Differential_0_1();
-    int16_t readADC_Differential_0_3();
-    int16_t readADC_Differential_1_3();
-    int16_t readADC_Differential_2_3();
-    void startComparator_SingleEnded(uint8_t channel, int16_t threshold);
-    int16_t getLastConversionResults();
-    float computeVolts(int16_t counts);
-    void setGain(adsGain_t gain);
-    adsGain_t getGain();
-    void setDataRate(uint16_t rate);
-    uint16_t getDataRate();
-
-    void startADCReading(uint16_t mux, bool continuous);
-
-    bool conversionComplete();
+  bool conversionComplete();
 
 private:
-    void writeRegister(uint8_t reg, uint16_t value);
-    uint16_t readRegister(uint8_t reg);
-    uint8_t buffer[3];
+  void writeRegister(uint8_t reg, uint16_t value);
+  uint16_t readRegister(uint8_t reg);
+  uint8_t buffer[3];
 };
 
 /**************************************************************************/
@@ -190,9 +188,9 @@ private:
     @brief  Sensor driver for the Adafruit ADS1015 ADC breakout.
 */
 /**************************************************************************/
-class Adafruit_ADS1015: public Adafruit_ADS1X15 {
+class Adafruit_ADS1015 : public Adafruit_ADS1X15 {
 public:
-    Adafruit_ADS1015();
+  Adafruit_ADS1015();
 };
 
 /**************************************************************************/
@@ -200,9 +198,9 @@ public:
     @brief  Sensor driver for the Adafruit ADS1115 ADC breakout.
 */
 /**************************************************************************/
-class Adafruit_ADS1115: public Adafruit_ADS1X15 {
+class Adafruit_ADS1115 : public Adafruit_ADS1X15 {
 public:
-    Adafruit_ADS1115();
+  Adafruit_ADS1115();
 };
 
 #endif
